539e9222af4d4ce79e8899b869ddc4b6f7e424b1
Deepak Guptha <vzan2012@gmail.com>
2020-05-17 15:19:06 +0530

    feat: Data Validation and Sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const connectionURL = "mongodb://127.0.0.1:27017/task-manager-api";
 
@@ -8,18 +9,44 @@ mongoose.connect(connectionURL, {
 });
 
 // Creation of User
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String,
-//   },
-//   age: {
-//     type: Number,
-//   },
-// });
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) throw new Error("Email is Invalid");
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password"))
+        throw new Error("Please give any word other than Password");
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) throw new Error("Age must be a +ve number");
+    },
+  },
+});
 
 // const myself = new User({
-//   name: "vzan2012",
-//   age: 36,
+//   name: "               vzan2012",
+//   email: "     VZAN2012@gmail.com",
+//   password: "                    Pass9word",
 // });
 
 // myself
@@ -31,15 +58,17 @@ mongoose.connect(connectionURL, {
 const Tasks = mongoose.model("Tasks", {
   description: {
     type: String,
+    required: true,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
 const myTask = new Tasks({
-  description: "Tea Break after 1 hour",
-  completed: false,
+  description: "    Tea break at 4 or 5pm      ",
 });
 
 myTask
