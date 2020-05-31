79115cd97a8b85cd3103c40bd259e42f12cbe97b
Deepak Guptha <vzan2012@gmail.com>
2020-05-31 14:18:26 +0530

    feat: Updation of Resources Delete Endpoints

--- src/index.js
+++ src/index.js
@@ -86,6 +86,17 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+// Delete User by Id
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).send();
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 // Task Endpoint
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
@@ -136,6 +147,38 @@ app.get("/tasks/:id", async (req, res) => {
   //     res.send(task);
   //   })
   //   .catch((e) => res.status(500).send());
+});
+
+// Update Task by Id
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["completed", "description"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation)
+    return res.status(400).send({ error: "Invalid Operations !!!" });
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) return res.status(404).send();
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// Delete Task by Id
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).send();
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
