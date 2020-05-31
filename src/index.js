5cd2ec2948f96335d60c831caad095ae499962a5
Deepak Guptha <vzan2012@gmail.com>
2020-05-31 15:50:50 +0530

    refactor: Removed the un-used model declaration
    Closes closed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 require("./db/mongoose");
 
-const Task = require("./models/task");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
