4ae27fa2c0ad3e82da0111ae2d508ba877f10732
Deepak Guptha <vzan2012@gmail.com>
2020-06-13 19:05:57 +0530

    feat: Generation of token for login and create user

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -35,7 +36,23 @@ const userSchema = new mongoose.Schema({
       if (value < 0) throw new Error("Age must be a +ve number");
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "thisismyNodeCourse");
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
--- src/routers/user.js
+++ src/routers/user.js
@@ -5,10 +5,10 @@ const router = new express.Router();
 // User Endpoint
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
-
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -26,6 +26,9 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
+
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
 
     res.send(user);
   } catch (e) {
