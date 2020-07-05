fdce8762ea03c0e3afea1529fcff78bc0f8b63d7
Deepak Guptha <vzan2012@gmail.com>
2020-07-05 19:36:52 +0530

    feat: Sort Data by query options

--- src/routers/task.js
+++ src/routers/task.js
@@ -24,13 +24,20 @@ router.post("/tasks", auth, async (req, res) => {
 // Read Tasks
 // GET /tasks?completed-true
 // GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
   try {
     // const tasks = await Task.find({ owner: req.user._id });
     const match = {};
+    const sort = {};
 
     if (req.query.completed) {
       match.completed = req.query.completed === "true";
+    }
+
+    if (req.query.sortBy) {
+      const parts = req.query.sortBy.split(":");
+      sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
     }
 
     await req.user
@@ -40,6 +47,7 @@ router.get("/tasks", auth, async (req, res) => {
         options: {
           limit: parseInt(req.query.limit),
           skip: parseInt(req.query.skip),
+          sort,
         },
       })
       .execPopulate();
