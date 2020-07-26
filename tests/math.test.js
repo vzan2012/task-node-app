d28e9e7ebe292ecfe5569c8880ceba27f44bed64
Deepak Guptha <vzan2012@gmail.com>
2020-07-26 20:10:53 +0530

    test: Creating example test cases

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,5 +1,29 @@
-test("Hello World", () => {});
+const {
+  calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit,
+} = require("../src/math");
 
-test("This should fail !!!", () => {
-  throw new Error("FAILURE");
+// test("Hello World", () => {});
+
+// test("This should fail !!!", () => {
+//   throw new Error("FAILURE");
+// });
+
+test("Should calculate total with tip", () => {
+  const total = calculateTip(10, 0.3);
+
+  expect(total).toBe(13);
 });
+
+test("Should calculate total with default tip", () => {
+  const total = calculateTip(10);
+
+  expect(total).toBe(12.5);
+});
+
+test("Should convert 32 F to 0 C", () =>
+  expect(fahrenheitToCelsius(32)).toBe(0));
+
+test("Should convert 0 C to 32 F", () =>
+  expect(celsiusToFahrenheit(0)).toBe(32));
